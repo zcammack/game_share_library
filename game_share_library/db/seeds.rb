131e9a8d8eca7e4d476702c2ec0bf51575cef393
zcammack <zack.cammack@gmail.com>
2020-10-09 10:07:28 -0500

    Fixed some spelling errors in the seed file, as well as read further about properly using the Faker gem.

--- game_share_library/db/seeds.rb
+++ game_share_library/db/seeds.rb
@@ -1,19 +1,19 @@
 5.times do 
-    User.create(username: Dummy::name.name, email: Dummy::Internet.email, password: "Password")
+    User.create(username: Faker::name.name, email: Faker::Internet.email, password: "Password")
 end
 
 5.times do 
-    Game.create(title: NotARealGame::Game.title, platform: NotARealGame::Console.platform, user_id: 1)
+    Game.create(title: Faker::Game.title, platform: Faker::Game.platform, user_id: 1)
 end
 
 5.times do 
-    Game.create(title: NotARealGame::Game.title, platform: NotARealGame::Console.platform, user_id: 2)
+    Game.create(title: Faker::Game.title, platform: Faker::Game.platform, user_id: 2)
 end
 
 5.times do 
-    Game.create(title: NotARealGame::Game.title, platform: NotARealGame::Console.platform, user_id: 3)
+    Game.create(title: Faker::Game.title, platform: Faker::Game.platform, user_id: 3)
 end
 
 5.times do 
-    Game.create(title: NotARealGame::Game.title, platform: NotARealGame::Console.platform, user_id: 4)
+    Game.create(title: Faker::Game.title, platform: Faker::Game.platform, user_id: 4)
 end
