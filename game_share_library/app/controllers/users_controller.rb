771ce1e169ba251a9b1b1be355aa9dcf8d0159b1
zcammack <zack.cammack@gmail.com>
2020-10-10 11:34:18 -0500

    Created user controller.

--- game_share_library/app/controllers/users_controller.rb
+++ game_share_library/app/controllers/users_controller.rb
@@ -1,37 +1,17 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get '/users/signup' do
+    erb :"users/signup"
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
-  end
-
-  # POST: /users
-  post "/users" do
-    redirect "/users"
-  end
-
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
+  post '/users' do
+    @user = User.new(params)
+    
+    if @user && @user.save
+      session[:user_id] = @user.id
+      redirect "/games"
+    else
+      erb :"users/signup"
+    end
   end
 end
