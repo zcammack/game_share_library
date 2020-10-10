94f65036a9b830e2677d8e400e3482bc7e2c5d5b
zcammack <zack.cammack@gmail.com>
2020-10-10 16:56:25 -0500

    Built out application controller. Added multiple helper methods to help with log in controls.

--- game_share_library/app/controllers/application_controller.rb
+++ game_share_library/app/controllers/application_controller.rb
@@ -5,10 +5,37 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV["SECRET"]
+    register Sinatra::Flash
   end
 
   get "/" do
-    erb :welcome
+    redirect_if_logged_in
+    redirect "/login"
+  end
+
+  helpers do
+    def logged_in?
+      session[:user_id]
+    end
+
+    def current_user
+      @user ||= User.find_by(id: session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:errors] = "You need to log in to view this page."
+        redirect "/login"
+      end
+    end
+
+    def redirect_if_logged_in
+      if logged_in?
+        redirect "/games"
+      end
+    end
   end
 
 end
